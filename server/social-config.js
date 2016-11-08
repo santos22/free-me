b17251699fa85f081dfe469cb6abcf8fda772a35
Santos Solorzano <santosjavier22@gmail.com>
2016-11-07 22:11:57 -0600

    Delete social

--- server/social-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
- 
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '954628641308926',
-    secret: '145339de80c6ef5e1edf365e8222d525'
-});
