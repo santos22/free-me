44c5ffc99fd1e1e3bdfe905f2525bf8a021954b8
Santos Solorzano <santosjavier22@gmail.com>
2016-11-07 22:13:24 -0600

    social

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '954628641308926',
+    secret: '145339de80c6ef5e1edf365e8222d525'
+});
